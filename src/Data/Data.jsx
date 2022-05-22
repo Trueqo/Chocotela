7a47e34f819d766f3332f40bda35a892b55f716e
Trueqo <julianochoa1999@gmail.com>
2022-05-22 16:31:01 -0500

    card ready

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -2,37 +2,37 @@ const products =[
     {
         id:1,
         Image:'/assets/imgSlider/img1.jpg',
-        title:'product',
-        price:'500',
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        title:'Fresita decorada',
+        price:'1000 unidad',
+        description:'Magnifica fresita cubierta con chocolate blanco o negro y decorada .'
     },
     {
         id:2,
         Image:'/assets/imgSlider/img2.jpg',
-        title:'product2',
-        price:'500',
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        title:'Estrellas en palito',
+        price:'500 unidad',
+        description:'Ricas estrellas en palito, con una no quedas satisfech@.'
     },
     {
         id:3,
         Image:'/assets/imgSlider/img3.jpg',
-        title:'product3',
-        price:'500',
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        title:'Corazón en palito',
+        price:'700 unidad',
+        description:'Delicioso corazón en palito, ideal para calmar antojos de chocolate.'
     },
     {
         id:4,
         Image:'/assets/imgSlider/img4.jpg',
-        title:'product4',
-        price:'500',
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        title:'Caja de fresitas',
+        price:'5000 unidad',
+        description:'Rica caja de fresas cubiertas con chocolate y decoradas.'
     },
     {
         id:5,
         Image:'/assets/imgSlider/img5.jpg',
-        title:'product5',
-        price:'500',
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        title:'Rosas decoradas',
+        price:'1200 unidad',
+        description:'Deliciosa de chocolate blanco y negro.'
     }]
 
 export default products
--- src/Views/ProductsView.jsx
+++ src/Views/ProductsView.jsx
@@ -3,17 +3,17 @@ import products from '../Data/Data'
 
 const ProductsView = () => {
   return (
-    <div className='w-full h-auto bg-color4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 p-[4rem]'>
+    <div className='w-full h-auto bg-color4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-[1rem]'>
        
             {products.map(datos=>(
-                <div className='bg-red-500  w-[15rem] h-[20rem] flex flex-col items-center '>
-                    <div className=' w-[10rem] h-[10rem]'>
-                        <img src={datos.Image}  className='w-[10rem] h-[10rem] object-fill'/>
+                <div className='bg-color5 rounded-2xl w-[15rem] h-[20rem] flex flex-col items-center flex-wrap mx-auto my-6'>
+                    <div className=' w-full h-[10rem] '>
+                        <img src={datos.Image}  className='w-full h-[10rem] object-cover rounded-t-2xl'/>
                     </div>
-                    <span>{datos.title}</span>
+                    <span className='text-xl font-[nunito]'>{datos.title}</span>
                     <div className='flex flex-row flex-wrap justify-center text-center'>
-                        <span>{datos.price}</span>
-                        <span className=''>{datos.description}</span>
+                        <span className='font-[roboto]'> Valor: {datos.price}</span>
+                        <span className='bg-color6 mx-4 text-black font-[roboto] rounded-xl'>{datos.description}</span>
                     </div>
                 </div>
             ))}
