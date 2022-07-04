86f23aef177900b2af46cb1fe693b3a085d59127
Trueqo <julianochoa1999@gmail.com>
2022-07-04 16:56:40 -0500

    update home

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -2,9 +2,19 @@ import Slider from "../Components/Carousel/Slider"
 
 const Home = () => {
     return (
-        <div className='h-screen bg-orange-900 grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 '>
+        <div className='h-screen bg-orange-900 grid grid-rows-2 grid-cols-1 md:grid-cols-2 md:grid-rows-1'>
+                <div className="bg-green-300 ">
+            
+                </div>
                 
-            <div className=' md:w-[100%] md:h-[100%] row-span-1 md:col-span-1'>
+                <div className='w-[100%] h-[100%] relative flex items-center justify-center'>
+                    <img src='/assests/homeImg1.png' className='w-[85%] h-[90%] absolute bottom-0' />
+                </div>
+                
+
+
+
+            {/* <div className=' md:w-[100%] md:h-[100%] row-span-1 md:col-span-1'>
                 <div className="w-full h-full  flex pt-[6rem]
                 justify-center">
                     <Slider/>
@@ -12,7 +22,7 @@ const Home = () => {
             </div>
             <div className='w-[100%] h-[100%] relative flex items-center justify-center'>
                 <img src='/assests/homeImg1.png' className='w-[85%] h-[90%] absolute bottom-0' />
-            </div>
+            </div> */}
         </div>
     )
 }
