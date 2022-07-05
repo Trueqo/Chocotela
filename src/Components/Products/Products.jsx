5bd3df14ba04b8d4ec7de73ffe33243acc36c4e4
Trueqo <julianochoa1999@gmail.com>
2022-07-04 21:14:10 -0500

    bug with routes

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -6,14 +6,14 @@ const Products = () => {
   const{addItemToCart} = useContext(CartContext)
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pt-[4rem] pb-[4rem] gap-3 '>
+    <div className='bg-color5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pt-[4rem] pb-[4rem] gap-3 '>
     {products.map((product,i) => (
-      <div key={i} className='w-[10rem] h-auto m-auto flex flex-col items-center  bg-white rounded-2xl
-      '>
+      <div key={i} className='w-[10rem] h-auto m-auto flex flex-col items-center rounded-2xl
+      bg-color4'>
         <img src={product.Image} className='w-[10rem] h-[8rem] rounded-t-2xl'/>
         <h1>{product.name}</h1>
         <p className='w-[9rem] h-auto py-1 text-center '>{product.description}</p>
-        <p>$ <span className='text-blue-500'>{product.price}</span></p>
+        <p>$ <span className='text-green-500'>{product.price}</span></p>
         <button onClick={()=> addItemToCart(product)}
         className='bg-green-500 rounded-xl px-2 py-1 mb-2'>
           add to cart
